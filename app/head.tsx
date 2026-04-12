e49c518f9caaa4c802c6601f51d802c52d3c0b76
vinsliu626 <gcswzh@gmail.com>
2026-04-12 01:52:19 -0500

    vertifly

---
+++ app/head.tsx
@@ -0,0 +1,10 @@
+export default function Head() {
+  return (
+    <>
+      <meta
+        name="google-site-verification"
+        content="-V_q2yY-OlZKMUhdL8jAvanAfn1_EIhnFzWLdL-0oRc"
+      />
+    </>
+  );
+}
